5d57c57e74387fd5c7ac826998c8d07006042fd9
Camden_Shaw <camden.shaw@gmail.com>
2024-05-26 16:52:36 -0700

    Did the exercises for Order Precedence.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,31 @@ namespace NumbersInCSharp
         static void Main(string[] args)
         {
             WorkWithIntegers();
-            
+            OrderPrecedence();
+
+            void OrderPrecedence() {
+                int a = 5;
+                int b = 4;
+                int c = 2;
+                int d = a + b * c;
+                Console.WriteLine(d);
+
+                d = (a + b) * c;
+                Console.WriteLine(d);
+
+                d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+                Console.WriteLine(d);
+
+                d = (a + c) * 4 / a * (a / b) - 14 + 7 - (7 * 3 - (4 - (5 + 6)));
+                Console.WriteLine(d);
+
+                int e = 7;
+                int f = 4;
+                int g = 3;
+                int h = (e + f) / g;
+                Console.WriteLine(h);
+            }
+
             void WorkWithIntegers() {
                 int a = 18;
                 int b = 6;
