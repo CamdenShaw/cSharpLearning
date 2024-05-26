3a976866e1796f1d386c06004386b95f04893289
Camden_Shaw <camden.shaw@gmail.com>
2024-05-26 14:26:57 -0700

    Performed some math experiments per the lesson plan on learn.microsoft.com.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace NumbersInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+
+            // other experiments
+            c = a + b - 12 * 17;
+            Console.WriteLine(c);
+            
+            double d = a - b * 122 / 7 - Math.PI;
+            Console.WriteLine(d);
         }
     }
 }
