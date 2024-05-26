0c5d1e8e85ef0fa3f4a584f8a2ffe1c8bde2ef7a
Camden_Shaw <camden.shaw@gmail.com>
2024-05-26 16:44:21 -0700

    Restructured the code so the integers work is in its own methods.

--- Program.cs
+++ Program.cs
@@ -6,28 +6,32 @@ namespace NumbersInCSharp
     {
         static void Main(string[] args)
         {
-            int a = 18;
-            int b = 6;
-            int c = a + b;
-            Console.WriteLine(c);
-            // subtraction
-            c = a - b;
-            Console.WriteLine(c);
-
-            // multiplication
-            c = a * b;
-            Console.WriteLine(c);
-
-            // division
-            c = a / b;
-            Console.WriteLine(c);
-
-            // other experiments
-            c = a + b - 12 * 17;
-            Console.WriteLine(c);
+            WorkWithIntegers();
             
-            double d = a - b * 122 / 7 - Math.PI;
-            Console.WriteLine(d);
+            void WorkWithIntegers() {
+                int a = 18;
+                int b = 6;
+                int c = a + b;
+                Console.WriteLine(c);
+                // subtraction
+                c = a - b;
+                Console.WriteLine(c);
+
+                // multiplication
+                c = a * b;
+                Console.WriteLine(c);
+
+                // division
+                c = a / b;
+                Console.WriteLine(c);
+
+                // other experiments
+                c = a + b - 12 * 17;
+                Console.WriteLine(c);
+                
+                double d = a - b * 122 / 7 - Math.PI;
+                Console.WriteLine(d);
+            }
         }
     }
 }
