45b4686498b21926815070a11f3907ae57943072
Camden_Shaw <camden.shaw@gmail.com>
2024-06-09 14:44:35 -0700

    Restored the output from the first part of the lesson.

--- IfsAndLoops.cs
+++ IfsAndLoops.cs
@@ -6,12 +6,21 @@ namespace IfsAndLoops
     {
         public static void MainLogic()
         {
-            runIfs();
+            ExploreIf();
 
-            void runIfs()
+            void ExploreIf()
             {
                 int a = 5;
                 int b = 3;
+                if (a + b > 10)
+                {
+                    Console.WriteLine("The answer is greater than 10.");
+                }
+                else
+                {
+                    Console.WriteLine("The answer is not greater than 10.");
+                }
+
                 int c = 4;
                 if ((a + b + c > 10) && (a == b))
                 {
