0c1f4000944f246fcb56e3f1aa40e0d2d4dbf6f0
Camden_Shaw <camden.shaw@gmail.com>
2024-06-09 15:15:18 -0700

    Wrote while and do-while loops.

--- IfsAndLoops.cs
+++ IfsAndLoops.cs
@@ -6,7 +6,8 @@ namespace IfsAndLoops
     {
         public static void MainLogic()
         {
-            ExploreIf();
+            // ExploreIf();
+            ExploreLoops();
 
             void ExploreIf()
             {
@@ -43,6 +44,23 @@ namespace IfsAndLoops
                     Console.WriteLine("The answer is not greater than 10");
                     Console.WriteLine("Or the first number is not greater than the second");
                 }
+            }
+
+            void ExploreLoops()
+            {
+                int counter = 0;
+                while (counter < 10)
+                {
+                    Console.WriteLine($"Hello World! The counter is {counter}");
+                    counter++;
+                }
+
+                counter = 0;
+                do
+                {
+                    Console.WriteLine($"Hello World! The counter is {counter}");
+                    counter++;
+                } while (counter < 10);
             }
         }
     }
