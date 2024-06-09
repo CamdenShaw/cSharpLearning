f2499b4613849ae8970c1df37f37fb674cafc21d
Camden_Shaw <camden.shaw@gmail.com>
2024-06-09 15:27:41 -0700

    Reversed the nesting of loops per the instructions.

--- IfsAndLoops.cs
+++ IfsAndLoops.cs
@@ -76,12 +76,12 @@ namespace IfsAndLoops
 
             void ExploreNestedLoops()
             {
-                for (int row = 1; row < 11; row++)
+                for (char column = 'a'; column < 'k'; column++)
                 {
-                    Console.WriteLine($"The row is {row}");
-                    for (char column = 'a'; column < 'k'; column++)
+                    Console.WriteLine($"The column is {column}");
+                    for (int row = 1; row < 11; row++)
                     {
-                        Console.WriteLine($"The column is {column}");
+                        Console.WriteLine($"The row is {row}");
                         Console.WriteLine($"The cell is {row}, {column}");
                     }
                 }
