d92a8a486f6ad76606dc911f0cc870ce0e90addf
Camden_Shaw <camden.shaw@gmail.com>
2024-06-09 14:34:46 -0700

    The answer is not greater than 10.

--- IfsAndLoops.cs
+++ IfsAndLoops.cs
@@ -11,7 +11,7 @@ namespace IfsAndLoops
             void runIfs()
             {
                 int a = 5;
-                int b = 6;
+                int b = 3;
                 if (a + b > 10)
                     Console.WriteLine("The answer is greater than 10.");
             }
